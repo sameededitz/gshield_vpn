e407622e9a7b98a45b9d2de2b6474096bb96bf69
Azil6744 <developmentwithazil@gmail.com>
2025-05-08 18:16:24 +0500

    fix: update image paths and manifest for improved asset management

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,6 +7,11 @@
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon-16x16.png') }}">
 <link rel="manifest" href="{{ asset('assets/images/site.webmanifest') }}">
+
+{{-- <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest"> --}}
 <!--plugins-->
 <link href="{{ asset('assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
