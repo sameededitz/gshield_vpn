ef714b4cdab5d3353765003cd28179d12828deed
Sameed <sameedhassan22@gmail.com>
2025-05-12 19:13:57 +0500

    feat: add gshieldvpn.com to allowed origins in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://gshieldvpn.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
